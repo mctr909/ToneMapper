cf412288cf21ff18d2d5e6069e043c6c9ea3d66d
mctr909 <tr-909@hotmail.co.jp>
2021-01-27 09:45:18 +0900

    Add files via upload

---
+++ ToneMapper/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ToneMapper {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tscLayer = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.pnlRegion = new System.Windows.Forms.Panel();
+            this.hScroll = new System.Windows.Forms.HScrollBar();
+            this.vScroll = new System.Windows.Forms.VScrollBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picRegion = new System.Windows.Forms.PictureBox();
+            this.tsbWrite = new System.Windows.Forms.ToolStripButton();
+            this.tsbSelect = new System.Windows.Forms.ToolStripButton();
+            this.tsbEnableVelo = new System.Windows.Forms.ToolStripButton();
+            this.tsbToneZoom = new System.Windows.Forms.ToolStripButton();
+            this.tsbToneZoomout = new System.Windows.Forms.ToolStripButton();
+            this.tsbVeloZoom = new System.Windows.Forms.ToolStripButton();
+            this.tsbVeloZoomout = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.pnlRegion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRegion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tscLayer,
+            this.toolStripSeparator1,
+            this.toolStripSeparator2,
+            this.tsbWrite,
+            this.tsbSelect,
+            this.toolStripSeparator3,
+            this.tsbEnableVelo,
+            this.toolStripSeparator4,
+            this.tsbToneZoom,
+            this.tsbToneZoomout,
+            this.tsbVeloZoom,
+            this.tsbVeloZoomout});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(365, 31);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tscLayer
+            // 
+            this.tscLayer.AutoSize = false;
+            this.tscLayer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tscLayer.Items.AddRange(new object[] {
+            "レイヤー1",
+            "レイヤー2",
+            "レイヤー3",
+            "レイヤー4",
+            "レイヤー5",
+            "レイヤー6",
+            "レイヤー7",
+            "レイヤー8"});
+            this.tscLayer.Name = "tscLayer";
+            this.tscLayer.Size = new System.Drawing.Size(75, 23);
+            this.tscLayer.SelectedIndexChanged += new System.EventHandler(this.tscLayer_SelectedIndexChanged);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 31);
+            // 
+            // pnlRegion
+            // 
+            this.pnlRegion.BackColor = System.Drawing.Color.Silver;
+            this.pnlRegion.Controls.Add(this.hScroll);
+            this.pnlRegion.Controls.Add(this.vScroll);
+            this.pnlRegion.Controls.Add(this.picRegion);
+            this.pnlRegion.Location = new System.Drawing.Point(12, 34);
+            this.pnlRegion.Name = "pnlRegion";
+            this.pnlRegion.Size = new System.Drawing.Size(313, 212);
+            this.pnlRegion.TabIndex = 1;
+            // 
+            // hScroll
+            // 
+            this.hScroll.Location = new System.Drawing.Point(3, 195);
+            this.hScroll.Maximum = 127;
+            this.hScroll.Name = "hScroll";
+            this.hScroll.Size = new System.Drawing.Size(290, 17);
+            this.hScroll.TabIndex = 2;
+            // 
+            // vScroll
+            // 
+            this.vScroll.Location = new System.Drawing.Point(296, 4);
+            this.vScroll.Name = "vScroll";
+            this.vScroll.Size = new System.Drawing.Size(17, 188);
+            this.vScroll.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // picRegion
+            // 
+            this.picRegion.BackColor = System.Drawing.Color.White;
+            this.picRegion.Location = new System.Drawing.Point(3, 3);
+            this.picRegion.Name = "picRegion";
+            this.picRegion.Size = new System.Drawing.Size(290, 189);
+            this.picRegion.TabIndex = 0;
+            this.picRegion.TabStop = false;
+            this.picRegion.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picRegion_MouseDown);
+            this.picRegion.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picRegion_MouseMove);
+            this.picRegion.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picRegion_MouseUp);
+            // 
+            // tsbWrite
+            // 
+            this.tsbWrite.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbWrite.Image = global::ToneMapper.Properties.Resources.write;
+            this.tsbWrite.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbWrite.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbWrite.Name = "tsbWrite";
+            this.tsbWrite.Size = new System.Drawing.Size(28, 28);
+            this.tsbWrite.Text = "toolStripButton2";
+            this.tsbWrite.Click += new System.EventHandler(this.tsbWrite_Click);
+            // 
+            // tsbSelect
+            // 
+            this.tsbSelect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSelect.Image = global::ToneMapper.Properties.Resources.select_disable;
+            this.tsbSelect.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbSelect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSelect.Name = "tsbSelect";
+            this.tsbSelect.Size = new System.Drawing.Size(29, 28);
+            this.tsbSelect.Text = "toolStripButton1";
+            this.tsbSelect.Click += new System.EventHandler(this.tsbSelect_Click);
+            // 
+            // tsbEnableVelo
+            // 
+            this.tsbEnableVelo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbEnableVelo.Image = global::ToneMapper.Properties.Resources.input_velo;
+            this.tsbEnableVelo.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbEnableVelo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEnableVelo.Name = "tsbEnableVelo";
+            this.tsbEnableVelo.Size = new System.Drawing.Size(28, 28);
+            this.tsbEnableVelo.Text = "強弱入力(ON)";
+            this.tsbEnableVelo.Click += new System.EventHandler(this.tsbEnableVelo_Click);
+            // 
+            // tsbToneZoom
+            // 
+            this.tsbToneZoom.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbToneZoom.Image = global::ToneMapper.Properties.Resources.tone_zoom;
+            this.tsbToneZoom.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbToneZoom.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbToneZoom.Name = "tsbToneZoom";
+            this.tsbToneZoom.Size = new System.Drawing.Size(28, 28);
+            this.tsbToneZoom.ToolTipText = "音程方向拡大";
+            this.tsbToneZoom.Click += new System.EventHandler(this.tsbToneZoom_Click);
+            // 
+            // tsbToneZoomout
+            // 
+            this.tsbToneZoomout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbToneZoomout.Image = global::ToneMapper.Properties.Resources.tone_zoomout;
+            this.tsbToneZoomout.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbToneZoomout.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbToneZoomout.Name = "tsbToneZoomout";
+            this.tsbToneZoomout.Size = new System.Drawing.Size(28, 28);
+            this.tsbToneZoomout.ToolTipText = "音程方向縮小";
+            this.tsbToneZoomout.Click += new System.EventHandler(this.tsbToneZoomout_Click);
+            // 
+            // tsbVeloZoom
+            // 
+            this.tsbVeloZoom.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbVeloZoom.Image = global::ToneMapper.Properties.Resources.velo_zoom;
+            this.tsbVeloZoom.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbVeloZoom.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbVeloZoom.Name = "tsbVeloZoom";
+            this.tsbVeloZoom.Size = new System.Drawing.Size(28, 28);
+            this.tsbVeloZoom.ToolTipText = "強弱方向拡大";
+            this.tsbVeloZoom.Click += new System.EventHandler(this.tsbVeloZoom_Click);
+            // 
+            // tsbVeloZoomout
+            // 
+            this.tsbVeloZoomout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbVeloZoomout.Image = global::ToneMapper.Properties.Resources.velo_zoomout;
+            this.tsbVeloZoomout.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbVeloZoomout.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbVeloZoomout.Name = "tsbVeloZoomout";
+            this.tsbVeloZoomout.Size = new System.Drawing.Size(28, 28);
+            this.tsbVeloZoomout.ToolTipText = "強弱方向縮小";
+            this.tsbVeloZoomout.Click += new System.EventHandler(this.tsbVeloZoomout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 258);
+            this.Controls.Add(this.pnlRegion);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.pnlRegion.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picRegion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox tscLayer;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton tsbToneZoom;
+        private System.Windows.Forms.ToolStripButton tsbToneZoomout;
+        private System.Windows.Forms.ToolStripButton tsbVeloZoom;
+        private System.Windows.Forms.ToolStripButton tsbVeloZoomout;
+        private System.Windows.Forms.Panel pnlRegion;
+        private System.Windows.Forms.HScrollBar hScroll;
+        private System.Windows.Forms.VScrollBar vScroll;
+        private System.Windows.Forms.PictureBox picRegion;
+        private System.Windows.Forms.ToolStripButton tsbEnableVelo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripButton tsbSelect;
+        private System.Windows.Forms.ToolStripButton tsbWrite;
+    }
+}
+
---
+++ ToneMapper/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ToneMapper {
+    public partial class Form1 : Form {
+        private static readonly int[] ToneWidth = new int[] { 8, 10, 12, 15, 20 };
+        private static readonly int[] VeloHeight = new int[] { 4, 6, 8, 10, 12, 15, 20 };
+        private static int mToneWidthIdx = 0;
+        private static int mVeloHeightIdx = 0;
+        private static int mToneWidth = ToneWidth[mToneWidthIdx];
+        private static int mVeloHeight = VeloHeight[mVeloHeightIdx];
+
+        private static readonly Brush CursorColor = new Pen(Color.FromArgb(127, 127, 211, 127), 1.0f).Brush;
+        private static readonly Pen Gray12 = new Pen(Color.FromArgb(48, 0, 0, 0), 1.0f);
+        private static readonly Pen SelectAreaColor = new Pen(Color.Blue, 1.0f);
+
+        private Font mFont = new Font("MS Gothic", 9.0f);
+        private Bitmap bmpRoll;
+        private Graphics gRoll;
+        private Point mCursor;
+        private int mDispVelos;
+        private int mToneBegin;
+        private int mToneEnd;
+        private int mVeloBegin;
+        private int mVeloEnd;
+        private bool mIsDrag;
+        private bool mEnableVelo = true;
+
+        public Form1() {
+            bmpRoll = new Bitmap(1, 1);
+            gRoll = Graphics.FromImage(bmpRoll);
+            InitializeComponent();
+            tscLayer.SelectedIndex = 0;
+            tsbWrite.Checked = true;
+            vScroll.Value = vScroll.Minimum < 92 ? 92 : vScroll.Minimum;
+
+            setSize();
+
+            timer1.Interval = 16;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e) {
+            setSize();
+        }
+
+        private void tscLayer_SelectedIndexChanged(object sender, EventArgs e) {
+            draw();
+        }
+
+        private void tsbSelect_Click(object sender, EventArgs e) {
+            tsbWrite.Image = Properties.Resources.write_disable;
+            tsbSelect.Image = Properties.Resources.select;
+            tsbWrite.Checked = false;
+            tsbSelect.Checked = true;
+        }
+
+        private void tsbWrite_Click(object sender, EventArgs e) {
+            tsbWrite.Image = Properties.Resources.write;
+            tsbSelect.Image = Properties.Resources.select_disable;
+            tsbWrite.Checked = true;
+            tsbSelect.Checked = false;
+        }
+
+        private void tsbEnableVelo_Click(object sender, EventArgs e) {
+            mEnableVelo = !mEnableVelo;
+            if (mEnableVelo) {
+                mVeloHeight = VeloHeight[mVeloHeightIdx];
+                tsbEnableVelo.ToolTipText = "強弱入力(ON)";
+                tsbEnableVelo.Image = Properties.Resources.input_velo;
+            } else {
+                mVeloHeight = 2;
+                tsbEnableVelo.ToolTipText = "強弱入力(OFF)";
+                tsbEnableVelo.Image = Properties.Resources.input_velo_disable;
+            }
+            setDispVelos(0);
+        }
+
+        private void tsbToneZoom_Click(object sender, EventArgs e) {
+            if (mToneWidthIdx < ToneWidth.Length - 1) {
+                mToneWidthIdx++;
+                mToneWidth = ToneWidth[mToneWidthIdx];
+            }
+            if (mToneWidthIdx == ToneWidth.Length - 1) {
+                tsbToneZoom.Enabled = false;
+            }
+            tsbToneZoomout.Enabled = true;
+            setFontSize();
+            draw();
+        }
+
+        private void tsbToneZoomout_Click(object sender, EventArgs e) {
+            if (0 < mToneWidthIdx) {
+                mToneWidthIdx--;
+                mToneWidth = ToneWidth[mToneWidthIdx];
+            }
+            if (0 == mToneWidthIdx) {
+                tsbToneZoomout.Enabled = false;
+            }
+            tsbToneZoom.Enabled = true;
+            setFontSize();
+            draw();
+        }
+
+        private void tsbVeloZoom_Click(object sender, EventArgs e) {
+            setDispVelos(1);
+            draw();
+        }
+
+        private void tsbVeloZoomout_Click(object sender, EventArgs e) {
+            setDispVelos(-1);
+            draw();
+        }
+
+        private void picRegion_MouseDown(object sender, MouseEventArgs e) {
+            mIsDrag = true;
+            mToneBegin = hScroll.Value + mCursor.X / mToneWidth;
+            var ofsY = bmpRoll.Height % mVeloHeight;
+            if (mEnableVelo) {
+                mVeloBegin = 127 + vScroll.Minimum - vScroll.Value - (mCursor.Y - ofsY) / mVeloHeight;
+            } else {
+                mVeloBegin = 0;
+            }
+        }
+
+        private void picRegion_MouseUp(object sender, MouseEventArgs e) {
+            mIsDrag = false;
+        }
+
+        private void picRegion_MouseMove(object sender, MouseEventArgs e) {
+            snapCursor(picRegion.PointToClient(Cursor.Position));
+            mToneEnd = hScroll.Value + mCursor.X / mToneWidth;
+            var ofsY = bmpRoll.Height % mVeloHeight;
+            if (mEnableVelo) {
+                mVeloEnd = 127 + vScroll.Minimum - vScroll.Value - (mCursor.Y - ofsY) / mVeloHeight;
+            } else {
+                mVeloEnd = 127;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            draw();
+        }
+
+        private void snapCursor(Point pos) {
+            mCursor.X = pos.X / mToneWidth * mToneWidth;
+            var tone = hScroll.Value + mCursor.X / mToneWidth;
+            if (127 < tone) {
+                var toneM = 127 - hScroll.Value;
+                mCursor.X = toneM * mToneWidth;
+            }
+            if (tone < 0) {
+                mCursor.X = 0;
+            }
+
+            if (picRegion.Width <= pos.X) {
+                pos.X = picRegion.Width - 1;
+                if (mIsDrag && hScroll.Value + hScroll.SmallChange < hScroll.Maximum) {
+                    hScroll.Value += hScroll.SmallChange;
+                }
+            }
+            if (pos.X < 0) {
+                pos.X = 0;
+                if (mIsDrag && 0 <= hScroll.Value - hScroll.SmallChange) {
+                    hScroll.Value -= hScroll.SmallChange;
+                }
+            }
+
+            var ofsY = bmpRoll.Height % mVeloHeight;
+            var height = mDispVelos * mVeloHeight + ofsY;
+            mCursor.Y = (pos.Y - ofsY) / mVeloHeight * mVeloHeight + ofsY;
+            if (height < mCursor.Y) {
+                mCursor.Y = height;
+            }
+
+            if (picRegion.Height <= pos.Y) {
+                pos.Y = picRegion.Height - 1;
+                if (mIsDrag && vScroll.Value < vScroll.Maximum) {
+                    vScroll.Value++;
+                }
+            }
+            if (pos.Y < 0) {
+                pos.Y = 0;
+                if (mIsDrag && vScroll.Minimum <= vScroll.Value - 1) {
+                    vScroll.Value--;
+                }
+            }
+        }
+
+        private void setDispVelos(int heightIncDec) {
+            if (0 < heightIncDec && mVeloHeightIdx < VeloHeight.Length - 1) {
+                mVeloHeightIdx++;
+                mVeloHeight = VeloHeight[mVeloHeightIdx];
+            }
+            if (heightIncDec < 0 && 0 < mVeloHeightIdx) {
+                mVeloHeightIdx--;
+                mVeloHeight = VeloHeight[mVeloHeightIdx];
+            }
+
+            if (mVeloHeightIdx == VeloHeight.Length - 1) {
+                tsbVeloZoom.Enabled = false;
+            } else {
+                tsbVeloZoom.Enabled = mEnableVelo;
+            }
+            if (0 == mVeloHeightIdx) {
+                tsbVeloZoomout.Enabled = false;
+            } else {
+                tsbVeloZoomout.Enabled = mEnableVelo;
+            }
+
+            mDispVelos = picRegion.Height / mVeloHeight;
+            if (128 < mDispVelos) {
+                mDispVelos = 128;
+            }
+            vScroll.Maximum = 128;
+            vScroll.Minimum = mDispVelos;
+            vScroll.SmallChange = 1;
+            vScroll.LargeChange = 1;
+        }
+
+        private void setSize() {
+            if (Height < toolStrip1.Height + hScroll.Height + 40) {
+                Height = toolStrip1.Height + hScroll.Height + 40;
+            }
+
+            pnlRegion.Left = 5;
+            pnlRegion.Top = toolStrip1.Bottom;
+            pnlRegion.Width = Width - 21;
+            pnlRegion.Height = Height - toolStrip1.Height - 39;
+
+            picRegion.Left = 0;
+            picRegion.Top = 0;
+            picRegion.Width = pnlRegion.Width - vScroll.Width;
+            picRegion.Height = pnlRegion.Height - hScroll.Height;
+
+            hScroll.Left = 0;
+            hScroll.Top = picRegion.Bottom;
+            hScroll.Width = picRegion.Width;
+
+            vScroll.Left = picRegion.Right;
+            vScroll.Top = 0;
+            vScroll.Height = picRegion.Height;
+
+            gRoll.Dispose();
+            bmpRoll = new Bitmap(picRegion.Width, picRegion.Height);
+            gRoll = Graphics.FromImage(bmpRoll);
+
+            setDispVelos(0);
+        }
+
+        private void setFontSize() {
+            int fsize = 7;
+            while (true) {
+                var newFont = new Font(mFont.Name, fsize);
+                if (16 < fsize || mToneWidth + 2 < gRoll.MeasureString("4", newFont).Width) {
+                    break;
+                }
+                mFont = newFont;
+                fsize++;
+            }
+        }
+
+        private void draw() {
+            var dispTones = picRegion.Width / mToneWidth;
+            var dispVelos = picRegion.Height / mVeloHeight;
+            var ofsY = bmpRoll.Height % mVeloHeight;
+            var height = mDispVelos * mVeloHeight + ofsY;
+
+            gRoll.Clear(Color.Transparent);
+
+            for (int i = 0, no = hScroll.Value; i < dispTones && no < 128; i++, no++) {
+                switch (no % 12) {
+                case 1:
+                case 3:
+                case 6:
+                case 8:
+                case 10:
+                    gRoll.FillRectangle(Brushes.LightGray, i * mToneWidth, 0, mToneWidth, height);
+                    gRoll.DrawLine(Pens.Gray, i * mToneWidth, 0, i * mToneWidth, height);
+                    break;
+                default:
+                    gRoll.DrawLine(Pens.Gray, i * mToneWidth, 0, i * mToneWidth, height);
+                    break;
+                }
+            }
+
+            if (mEnableVelo) {
+                for (int y = mDispVelos - 1, no = 128 - vScroll.Value; 0 <= y; y--, no++) {
+                    var py = mVeloHeight * y + ofsY;
+                    if (no % 16 == 0) {
+                        var name = no.ToString();
+                        var fsize = gRoll.MeasureString(name, mFont).Height - 2;
+                        gRoll.DrawLine(Pens.Gray, 0, py + mVeloHeight, bmpRoll.Width, py + mVeloHeight);
+                    } else {
+                        gRoll.DrawLine(Gray12, 0, py + mVeloHeight, bmpRoll.Width, py + mVeloHeight);
+                    }
+                }
+            }
+
+            if (mIsDrag) {
+                int x1 = (mToneBegin - hScroll.Value) * mToneWidth;
+                int x2 = (mToneEnd - hScroll.Value) * mToneWidth;
+                if (x2 < x1) {
+                    var tmp = x2;
+                    x2 = x1 + mToneWidth;
+                    x1 = tmp;
+                } else {
+                    x2 += mToneWidth;
+                }
+
+                int y1 = (127 - mVeloBegin - vScroll.Value + vScroll.Minimum) * mVeloHeight + ofsY;
+                int y2 = (127 - mVeloEnd - vScroll.Value + vScroll.Minimum) * mVeloHeight + ofsY;
+                if (y2 < y1) {
+                    var tmp = y2;
+                    y2 = y1 + mVeloHeight;
+                    y1 = tmp;
+                } else {
+                    y2 += mVeloHeight;
+                }
+
+                if (height < y2) {
+                    y2 = height;
+                }
+
+                if (tsbSelect.Checked) {
+                    gRoll.DrawRectangle(SelectAreaColor, x1, y1, x2 - x1, y2 - y1);
+                }
+                if (tsbWrite.Checked) {
+                    gRoll.FillRectangle(CursorColor, x1, y1, x2 - x1, y2 - y1);
+                    gRoll.DrawRectangle(Pens.Blue, x1, y1, x2 - x1, y2 - y1);
+                }
+            } else {
+                gRoll.FillRectangle(CursorColor, mCursor.X + 1, 0, mToneWidth - 1, height);
+                gRoll.DrawLine(Pens.Red, 0, mCursor.Y + mVeloHeight / 2, bmpRoll.Width, mCursor.Y + mVeloHeight / 2);
+            }
+
+            for (int i = 0, no = hScroll.Value; i < dispTones && no < 128; i++, no++) {
+                if (0 == no % 12) {
+                    gRoll.DrawString((no / 12 - 1).ToString(), mFont, Brushes.Black, i * mToneWidth, 0);
+                }
+            }
+
+            if (mEnableVelo) {
+                for (int y = mDispVelos - 1, no = 128 - vScroll.Value; 0 <= y; y--, no++) {
+                    if (no % 16 == 0) {
+                        var py = mVeloHeight * y + ofsY;
+                        var name = no.ToString();
+                        var fsize = gRoll.MeasureString(name, mFont).Height - 4;
+                        gRoll.DrawString(name, mFont, Brushes.Black, 0, py + mVeloHeight - fsize);
+                    }
+                }
+            }
+
+            picRegion.Image = bmpRoll;
+        }
+    }
+}
